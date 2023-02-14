ecddef0fcf1e16ec067947b386ced43b57d8ae4e
Mykola Yehorov <odesalover@gmail.com>
2023-02-14 13:01:38 +0200

    hw-26

--- main.js
+++ main.js
@@ -1,91 +1,110 @@
 "use strict"
 
-//1. Сума всіх елементів масиву
-let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//1. Об'єкт car
+let car = {
+  type: "electric",
+  wheels: 4
+};
 
-function sumArr(arr, sum = 0, i = 0) {
-    if (arr[i]) {
-		return sumArr(arr, sum + arr[i], ++i)
-	} else {
-		return sum
-	}
+let sportCar = {
+  doors: 2
+};
+
+let passengerCar = {
+  doors: 4
+};
+
+let toyCar = {
+  type: "toy"
+};
+
+Object.setPrototypeOf(sportCar, car);
+console.log(sportCar.wheels)
+
+Object.setPrototypeOf(passengerCar, car);
+console.log(passengerCar.type)
+
+Object.setPrototypeOf(toyCar, sportCar);
+console.log(toyCar.type)
+console.log(toyCar.wheels)
+
+
+//2. Виправити код. Я так зрозумів, що можна було будь-яким способом виправити код
+let frontendEmployees = {
+  wallet:{},
+  pay(munth, sum) {
+    this.wallet[munth] = sum;
+  }
+};
+
+let backendEmployees = {
+  wallet:{},
+  pay(munth, sum) {
+    this.wallet[munth] = sum;
+  }
+};
+
+let frontendDeveloper = {
+  name: 'Mike',
 }
-console.log('1. Сума всіх елементів масиву');
-console.log(sumArr(a));
+Object.setPrototypeOf(frontendDeveloper, frontendEmployees);
 
-//2. Кількість усіх елементів у масиві
-const arr1 = [];
-const arr2 = [1, 2, 3];
-const arr3 = ['x', 'y', ['z']];
-const arr4 = [1, 2, [3, 4, [5]]];
-const arr5 = [[[[]]]];
+let backendDeveloper = {
+  name: 'Bob',
+}
+Object.setPrototypeOf(backendDeveloper, backendEmployees);
+backendDeveloper.pay('june', 1500);
+frontendDeveloper.pay('june', 2000);
+console.log(backendDeveloper.wallet.june);
+console.log(frontendDeveloper.wallet.june);
 
-let count = 0;
+//3. Створити нового користувача
+function User(name, age){
+  this.name = name;
+  this.age = age
+}
+let user_1 = new User ('Mike', 18);
+let user_2 = new user_1.constructor ('Bob', 25);
+console.log(user_1);
+console.log(user_2);
 
-function deepCount(arr, reset = false) {
-  if(reset) count = 0
-  for (const item of arr) {
-    if (Array.isArray(item)) {
-      count++;
-      deepCount(item);
-    }else {
-      count++
+//4. Створити нового користувача. 1 варіант
+
+function UserType(name){
+  for(let i = 0; i < name.length; ++i){
+    this[i] = name[i];
+    if(i + 1 == name.length){
+      Object.defineProperty(this, 'length', {
+        enumerable: true,
+        writable: false,
+        configurable: true,
+        value: i + 1
+      });
     }
   }
-  return count
 }
+let admits = new UserType(['Mike', 'Bob', 'Nikola']);
 
-console.log('2. Кількість усіх елементів у масивах');
-console.log(deepCount(arr1, true));
-console.log(deepCount(arr2, true));
-console.log(deepCount(arr3, true));
-console.log(deepCount(arr4, true));
-console.log(deepCount(arr5, true));
+admits.join = Array.prototype.join;
+console.log(admits.join('; '));
 
-//3. Замикання
-let employees = {
-    development: {
-    backend: [{name: 'Mike', salary: 2000}, {name: 'Nikola', salary: 2500}],
-    frontend: [{name: 'Artem', salary: 3000}, {name: 'Alex', salary: 2700}],
-    },
-    sales: {
-        marketing: {
-            internet_marketers: [{name: 'Nina', salary: 1000}, {name: 'Olena', salary: 1300}],
-            promotion: [{name: 'Oleg', salary: 1400}, {name: 'Masha', salary: 1700}],
-            },
-        sellers: [{name: 'Max', salary: 900}, {name: 'Donald', salary: 700}, {name: 'Joe', salary: 1100}]
-        },
-    designer: [{name: 'Kate', salary: 1800}]
-}
 
-function takeSumSallary(){
-    let sum = 0;
-    return function sumSallary(obj)
-    {
-     for(let key in obj)
-        {
-            if (Array.isArray(obj[key]))
-                {
-                    obj[key].forEach(employee => sum += employee.salary);
-                } 
-            else 
-                {
-                    sumSallary(obj[key]);
-                }
-        }
-        return sum;
+//4. Створити нового користувача. 2 варіант
+function UserType2(name){
+  for(let i = 0; i < name.length; ++i){
+    this[i] = name[i];
+    if(i + 1 == name.length){
+      Object.defineProperty(this, 'length', {
+        enumerable: true,
+        writable: false,
+        configurable: true,
+        value: i + 1
+      });
     }
+  }
+  this.join = Array.prototype.join;
 }
-const sumSalary = takeSumSallary();
-console.log('3. Замикання. Сума зарплатні усіх працівників');
-console.log(sumSalary(employees));
+let admits2 = new UserType2(['Mike', 'Bob', 'Nikola']);
 
-//4. Найбільше число в трьох масивах
-let arrOne = [2, 15, 7 ,3];
-let arrTwo = [9, 3, 17, 12, 4, 8];
-let arrThree = [6, 11, 16, 15, 11];
+console.log(admits2.join('; '));
 
-let newArr = [...arrOne, ...arrTwo, ...arrThree];
-const maxNumber = Math.max(...newArr);
-console.log('4. Найбільше число в трьох масивах');
-console.log(maxNumber);
